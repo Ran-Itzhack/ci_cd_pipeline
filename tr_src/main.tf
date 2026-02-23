b4580364894a798e5661791f7e339d4c93a46f80
Ran Itzhack <ran.itzhack@gmail.com>
2026-02-23 13:58:49 -0800

    Deploy Docker to aws EC2

--- tr_src/main.tf
+++ tr_src/main.tf
@@ -41,35 +41,35 @@ module "ec2" {
 }
 
 # 1. Create the Role
-resource "aws_iam_role" "ssm_role" {
-  name = "ec2_ssm_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-    }]
-  })
-}
+# resource "aws_iam_role" "ssm_role" {
+#   name = "ec2_ssm_role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Principal = { Service = "ec2.amazonaws.com" }
+#     }]
+#   })
+# }
 
 # 2. Attach the SSM Managed Policy
-resource "aws_iam_role_policy_attachment" "ssm_attach" {
-  role       = aws_iam_role.ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
+# resource "aws_iam_role_policy_attachment" "ssm_attach" {
+#   role       = aws_iam_role.ssm_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+# }
 
 # 3. Create the Instance Profile
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "ec2_ssm_profile"
-  role = aws_iam_role.ssm_role.name
-}
+# resource "aws_iam_instance_profile" "ssm_profile" {
+#   name = "ec2_ssm_profile"
+#   role = aws_iam_role.ssm_role.name
+# }
 
 # 4. Add to your existing aws_instance resource
-resource "aws_instance" "ubuntu_ec2_instance_terraform" {
-  # ... your existing config ...
-  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
-}
+# resource "aws_instance" "ubuntu_ec2_instance_terraform" {
+#   # ... your existing config ...
+#   iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+# }
 
 
 data "aws_caller_identity" "current" {}
